7f34deb615253b906caa8428b3a8d620d8aa4d8f
Matt <mfxod828@gmail.com>
2019-09-26 20:43:54 -0400

    Login page test

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import Test from './components/Test'
-import logo from './logo.svg'
+import Login from './components/Login'
+import 'antd/dist/antd.css'
 import './App.css'
 
 function App () {
   return (
-    <div className='App'>
-      <Test />
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
     </div>
   )
 }
