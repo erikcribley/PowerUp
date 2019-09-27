483dd0c8d78df6d8ce614b09c5e0f2546c9bad2e
Matt <mfxod828@gmail.com>
2019-09-27 15:21:19 -0400

    added Characters component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Login from './components/Login'
+// import Login from './components/Login'
+import Characters from './components/Characters'
 import 'antd/dist/antd.css'
 import './App.css'
 
 function App () {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Characters />
     </div>
   )
 }
