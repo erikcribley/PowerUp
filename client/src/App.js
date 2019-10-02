006c92970230c5eb2fe3d74e896183e697fef693
Matt <mfxod828@gmail.com>
2019-10-01 20:40:25 -0400

    set up routes on exact paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
-// import Login from './components/Login'
-import Characters from './components/Characters'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Characters from "./pages/Characters"
+import StatsPage from "./pages/Stats"
+import Tasks from "./pages/Tasks"
+import Store from "./pages/Store"
+import Gameplay from "./pages/Gameplay"
 import 'antd/dist/antd.css'
 import './App.css'
 
-function App () {
+function App() {
   return (
-    <div>
-      {/* <Login /> */}
-      <Characters />
-    </div>
+    <Router>
+      <div>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/Characters" component={Characters} />
+          <Route exact path="/Stats" component={StatsPage} />
+          <Route exact path="/Tasks" component={Tasks} />
+          <Route exact path="/Store" component={Store} />
+          <Route exact path="/Gameplay" component={Gameplay} />
+      </div>
+    </Router>
   )
 }
 
