e9c0d01f9795e51a339d5bbcff4dd65943da4a38
Matt <mfxod828@gmail.com>
2019-10-03 19:10:36 -0400

    trying to get links working

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Menu } from 'antd'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 const { Header } = Layout
@@ -16,7 +17,7 @@ function TopNav() {
                 defaultSelectedKeys={['1']}
                 style={{ lineHeight: '64px' }}
             >
-                <Menu.Item key="1">Stats</Menu.Item>
+                <Menu.Item key="1" as={Link} to='/Stats'>Stats</Menu.Item>
                 <Menu.Item key="2">Tasks</Menu.Item>
                 <Menu.Item key="3">Store</Menu.Item>
                 <Menu.Item key="4">Gameplay</Menu.Item>
