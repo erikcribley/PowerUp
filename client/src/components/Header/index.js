84835715c2921b9c0b4c1af4fd80d31805bade40
Matt <mfxod828@gmail.com>
2019-10-08 19:56:58 -0400

    header now responsive

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import RightMenu from './right-menu'
-import { Drawer, Button } from 'antd'
+import { Drawer, Button, Menu } from 'antd'
 import './style.css'
 
 class Navbar extends Component {
   state = {
-    current: 'mail',
     visible: false
   }
 
@@ -15,40 +14,53 @@ class Navbar extends Component {
     })
   }
 
-onClose = () => {
-    this.setState({
-      visible: false,
-    })
+  onClose = () => {
+      this.setState({
+        visible: false,
+      })
   }
 
-render() {
-    return (
-        <nav className="menuBar">
-            <div className="logo">
-                <a href="/stats">
-                  <img src='./images/Power-Up-logo.png' alt='Power Up logo' />
-                </a>
-            </div>
-            <div className="menuCon">
-                <div className="rightMenu">
-                <RightMenu />
-                </div>
-                <Button className="barsMenu" type="primary" onClick={this.showDrawer}>
-                <span className="barsBtn"></span>
-                </Button>
-                <Drawer
-                title="Basic Drawer"
-                placement="right"
-                closable={false}
-                onClose={this.onClose}
-                visible={this.state.visible}
-                >
-                <RightMenu />
-                </Drawer>
-            </div>
-        </nav>
-    )
-  }
+  render() {
+      return (
+          <nav className="menuBar">
+              <div className="logo">
+                  <a href="/stats">
+                    <img src='./images/Power-Up-logo.png' alt='Power Up logo' />
+                  </a>
+              </div>
+              <div className="menuCon">
+                  <div className="rightMenu">
+                  <RightMenu />
+                  </div>
+                  <Button className="barsMenu" type="primary" onClick={this.showDrawer}>
+                  <span className="barsBtn"></span>
+                  </Button>
+                  <Drawer
+                  // title="Basic Drawer"
+                  placement="right"
+                  closable={false}
+                  onClose={this.onClose}
+                  visible={this.state.visible}
+                  >
+                    <Menu mode="vertical">
+                      <Menu.Item>
+                        <a href="/stats">Stats</a>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <a href="/tasks">Tasks</a>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <a href="/store">Store</a>
+                      </Menu.Item>
+                      <Menu.Item>
+                        <a href="/gameplay">Gameplay</a>
+                      </Menu.Item>
+                    </Menu>
+                  </Drawer>
+              </div>
+          </nav>
+      )
+    }
 }
 
 export default Navbar
