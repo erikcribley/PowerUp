5051e964bc21fd3e1f9a9ed367b48736a8c4c437
Matt <mfxod828@gmail.com>
2019-10-08 20:00:38 -0400

    all the adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Login from "./pages/Login"
+import Register from "./pages/Register"
 import Characters from "./pages/Characters"
 import StatsPage from "./pages/Stats"
 import Tasks from "./pages/Tasks"
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <div className='main-container'>
           <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
           <Route exact path="/characters" component={Characters} />
           <Route exact path="/stats" component={StatsPage} />
           <Route exact path="/tasks" component={Tasks} />
