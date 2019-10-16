95e0075acb4794211713d2cc1ef6fd5ac35c3798
Erik Cribley <erikcribley@gmail.com>
2019-10-15 21:46:37 -0400

    db for prompts

--- seedDB/seedData.sql
+++ seedDB/seedData.sql
@@ -31,7 +31,48 @@ VALUE
 (10, 10, 10, 50, 'Android Sphere', './images/sphere.svg'),
 (10, 10, 10, 50, 'Cyborg Craft', './images/craft.svg');
 
+//prompts
 USE `project3`;
-INSERT INTO `prompts` (`prompt`)
-VALUE
-('This is a test prompt');
+INSERT INTO `prompts` (`prompt`, `description`, `option1`, `option2`, `event1`, `event2`)
+VALUES
+	(
+		"You've recieved a distress signal from a nearby asteroid.", 
+        "1 -- Initial prompt",
+        "Respond", 
+        "Ignore",
+        "props.loadPrompt(2)", 
+        "props.loadPrompt(3)"
+	),
+    (
+		"It's an ambush, You're attacked by space pirates!",
+        "2 -- Respond to distress signal",
+        "Engage",
+        "Flee",
+        "4",
+        "5"
+	),
+    (
+		"Fuck that noise",
+		"3 -- ignore that shit",
+        "fly away",
+        "faster",
+        "69",
+        "68"
+	),
+    (
+		"Engage",
+        "4 -- engage",
+        "attack",
+        "raise shields",
+        "6",
+        "7"
+	),
+    (
+		"Super runaway",
+        "5 - GTFO",
+        "run",
+        "faster",
+        "8",
+        "9"
+	);
+
