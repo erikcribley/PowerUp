be66549e492fc92a54311e40ec4b452596a84970
Mitch Webster <mitch83083@gmail.com>
2019-10-18 18:58:26 -0400

    Created default tasks upon new account activation

--- orm/index.js
+++ orm/index.js
@@ -126,6 +126,21 @@ const orm = {
     })
   },
 
+  newUserTasks: userId => {
+    connection.query(
+      `INSERT INTO taskList(taskCredit, task, userId)
+      SELECT taskCredit, task, ?
+      FROM startingTasks`,
+      [userId],
+      (err, res) => {
+        if (err) {
+          return new Error(err)
+        }
+        return res.affectedRows
+      }
+    )
+  },
+
   // can be used to get any column out of users, playerShip, or taskList based on userId
   // just put the columns wanted into an array [U.userEmail, S.attack, T.task, etc]
   // can hard code these in if we want the same columns always or leave it to be selected at time of use
